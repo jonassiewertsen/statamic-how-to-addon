75e38773842a90e6b8abfe3fbcf5a94b58e8e20d
Jonas Siewertsen <mail@jonassiewertsen.com>
2019-12-22 23:52:20 +0100

    Cleaning up the videos controller

--- src/Http/Controllers/VideosController.php
+++ src/Http/Controllers/VideosController.php
@@ -2,9 +2,6 @@
 
 namespace Jonassiewertsen\Statamic\HowTo\Http\Controllers;
 
-
-use Statamic\Facades\Blueprint;
-use Statamic\Facades\Collection;
 use Statamic\Facades\Entry;
 
 class VideosController {
@@ -12,50 +9,5 @@ class VideosController {
         $videos = Entry::whereCollection('how_to_addon_videos');
 
         return view('howToAddon::videos.index', compact('videos'));
-    }
-
-    public function create()
-    {
-        // Get an array of values from the item that you want to be populated
-        // in the form. eg. ['title' => 'My Product', 'slug' => 'my-product']
-//        $values = $product->toArray();
-
-        // Get a blueprint. This might come from an actual blueprint yaml file
-        // or even defined in this class. Read more about blueprints below.
-        $blueprint = Blueprint::makeFromFields([
-            'title' => [
-                'type' => 'text',
-                'validate' => 'required',
-                'width' => 100,
-            ],
-            'handle' => [
-                'type' => 'text',
-                'validate' => 'required|alpha_dash',
-                'width' => 100,
-            ],
-        ]);
-
-        // Get a Fields object, a representation of the fields in a blueprint
-        // that factors in imported fieldsets, config overrides, etc.
-        $fields = $blueprint->fields();
-
-        // Add the values to the object. This will let you do things like
-        // validation, and processing, which is about to happen.
-//        $fields = $fields->addValues();
-
-        // Pre-process the values. This will convert the raw values into values
-        // that the corresponding fieldtype vue components will be expecting.
-//        $fields = $fields->preProcess();
-
-        // You'll probably prefer chaining all of that.
-        // $fields = $blueprint->fields()->addValues($values)->preProcess();
-
-        // The vue component will need these three values at a minimum.
-
-        return view('howToAddon::videos.create', [
-            'blueprint' => $blueprint->toPublishArray(),
-            'values'    => $fields->values(),
-            'meta'      => $fields->meta(),
-        ]);
     }
 }
