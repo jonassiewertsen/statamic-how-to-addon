197c6d1d7fd10e912811de8d0777e0f8127fa689
Jonas Siewertsen <mail@jonassiewertsen.com>
2019-12-23 15:13:32 +0100

    asset url not fixed yet

--- src/Http/Controllers/VideosController.php
+++ src/Http/Controllers/VideosController.php
@@ -15,6 +15,8 @@ class VideosController {
     public function show($slug) {
         $video = Entry::findBySlug($slug, config('howToAddon.blueprint.videos', 'how_to_addon_videos'));
 
+        // TODO: Should be replaced by Asset::findByPath later.
+        // https://github.com/statamic/cms/issues/1155
         $url = '/assets/' . $video->video;
 
         return view('howToAddon::videos.show', compact('video', 'url'));
