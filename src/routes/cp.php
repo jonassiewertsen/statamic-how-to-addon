54b362974f8bde9396a1ed9c151d49b071adce7f
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2020-04-26 20:09:24 +0200

    Compatible with Statamic beta 27

--- src/routes/cp.php
+++ src/routes/cp.php
@@ -1,5 +1,5 @@
 <?php
 
-Route::get('/how-to-addon/', 'Http\Controllers\VideosController@index')->name('howToAddon.index');
-Route::get('/how-to-addon/create', 'Http\Controllers\VideosController@create')->name('howToAddon.create');
-Route::get('/how-to-addon/video/{video}', 'Http\Controllers\VideosController@show')->name('howToAddon.show');
+Route::get('/how-to-addon/', 'VideosController@index')->name('howToAddon.index');
+Route::get('/how-to-addon/create', 'VideosController@create')->name('howToAddon.create');
+Route::get('/how-to-addon/video/{video}', 'VideosController@show')->name('howToAddon.show');
