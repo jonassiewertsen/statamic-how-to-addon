41b24086a962b0c225be721a088d00fcaa49e594
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2020-06-27 12:39:57 +0200

    Fix route names

--- src/routes/cp.php
+++ src/routes/cp.php
@@ -1,5 +1,5 @@
 <?php
 
 /** Videos */
-Route::get('/how-to/videos', 'VideosController@index')->name('howToAddon.videos.index');
-Route::get('/how-to/video/{video}', 'VideosController@show')->name('howToAddon.videos.show');
+Route::get('/how-to/videos', 'VideosController@index')->name('howToAddon.index');
+Route::get('/how-to/video/{video}', 'VideosController@show')->name('howToAddon.show');
